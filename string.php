c1405216fabfd716ec2868feba9a2afe1a05b90d
devNurul18 <162783746+devNurul18@users.noreply.github.com>
2024-05-17 19:52:53 +0600

    new commit

--- string.php
+++ string.php
@@ -1,2 +1,29 @@
 <?php
 
+$strings = ["Hello", "World", "PHP", "Programming"];
+
+function countVowels($str) 
+{
+    $str = strtolower($str); 
+    $vowelCount = 0; 
+  
+    for ($i = 0; $i < strlen($str); $i++) { 
+        if (in_array($str[$i], ["a", "e", "i", "o", "u"])) { 
+            $vowelCount++; 
+        } 
+    } 
+  
+    return $vowelCount; 
+} 
+  
+// Driver code 
+// $str = $strings[1]; 
+$vowelsCount = countVowels($strings[0]); 
+$vowelsCount2 = countVowels($strings[1]); 
+$vowelsCount3 = countVowels($strings[2]); 
+$vowelsCount4 = countVowels($strings[3]); 
+
+echo "original string: " .$strings[0] .", vowels: " .$vowelsCount." Reversed String:" . strrev($strings[0]) ."\n"; 
+echo "original string: " .$strings[1] .", vowels: " .$vowelsCount2." Reversed String:" . strrev($strings[1]) ."\n"; 
+echo "original string: " .$strings[2] .", vowels: " .$vowelsCount3." Reversed String:" . strrev($strings[2]) ."\n"; 
+echo "original string: " .$strings[2] .", vowels: " .$vowelsCount4." Reversed String:" . strrev($strings[3]) ."\n"; 
