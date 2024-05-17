c97cf4389213fe43c60a61b0538cc31db1c1da63
Jahid Hasan <jahidhasan370919@gmail.com>
2024-05-17 21:30:45 +0600

    extra prin_r removed

--- oop.php
+++ oop.php
@@ -1,121 +1,115 @@
 <?php
- class Book {
+class Book
+{
 
- //TODO: Add properties as Private
+  //TODO: Add properties as Private
 
-      private $title;
-      private $availableCopies;
- 
-   public function __construct($title, $availableCopies) {
+  private $title;
+  private $availableCopies;
 
-// TODO: Initialize properties
+  public function __construct($title, $availableCopies)
+  {
 
-     $this ->title = $title;
-     $this ->availableCopies = $availableCopies;
+    // TODO: Initialize properties
 
-     }
-
-
-// TODO: Add getTitle method
-
-     public function getTitle(){
-
-        return $this ->title;
-
-     }
- 
-
- // TODO: Add getAvailableCopies method
-    public function getAvailableCopies(){
-
-    return $this ->availableCopies;
-     }
-
- // TODO: Add borrowBook method
- 
-     public function borrowBook(){
-
-       if ($this -> availableCopies <=0){
-
-        throw new Exception("Library are empty you can't borrow");
-
-       }
-       else{
-        $this -> availableCopies --;
-       }
-       
-
-     }
+    $this->title = $title;
+    $this->availableCopies = $availableCopies;
+  }
 
 
- // TODO: Add returnBook method
+  // TODO: Add getTitle method
 
-     public function returnBook (){
+  public function getTitle()
+  {
 
-        $this -> availableCopies ++;
-
-     }
-
- 
-}
+    return $this->title;
+  }
 
 
-    class Member {
+  // TODO: Add getAvailableCopies method
+  public function getAvailableCopies()
+  {
 
- // TODO: Add properties as Private
+    return $this->availableCopies;
+  }
 
-     private $name;
+  // TODO: Add borrowBook method
 
- public function __construct($name) {
-      // TODO: Initialize properties
+  public function borrowBook()
+  {
 
-      $this -> name = $name;
+    if ($this->availableCopies <= 0) {
 
-     }
-
-
- // TODO: Add getName method
-
-      public function getName (){
-        return $this -> name;
-      }
-      
- 
- // TODO: Add borrowBook method
- 
-   public function borrowBook ($book){
-
-
-    $book -> borrowBook();
-
-   }
-
-
- // TODO: Add returnBook method
-
-
-    public function returnBook ($book){
-        $book -> returnBook();
+      throw new Exception("Library are empty you can't borrow");
+    } else {
+      $this->availableCopies--;
     }
- 
+  }
+
+
+  // TODO: Add returnBook method
+
+  public function returnBook()
+  {
+
+    $this->availableCopies++;
+  }
 }
 
 
-   $Book1 = new Book ("The Great Gatsby", 5);
-   $Book2 = new Book ("To Kill a Mockingbird", 3);
+class Member
+{
+
+  // TODO: Add properties as Private
+
+  private $name;
+
+  public function __construct($name)
+  {
+    // TODO: Initialize properties
+
+    $this->name = $name;
+  }
 
 
-   $Member1 = new Member("John Doe");
-   $Member2 = new Member("Jane Smith");
+  // TODO: Add getName method
+
+  public function getName()
+  {
+    return $this->name;
+  }
 
 
-   print_r ($Member1 ->borrowBook($Book1));
-   print_r ($Member2 ->borrowBook($Book2));
+  // TODO: Add borrowBook method
+
+  public function borrowBook($book)
+  {
 
 
-   print_r ("Available Copies of '{$Book1 ->getTitle()}': {$Book1 ->getAvailableCopies()} \n");
-   print_r ("Available Copies of '{$Book2 ->getTitle()}': {$Book2 ->getAvailableCopies()}");
+    $book->borrowBook();
+  }
 
 
+  // TODO: Add returnBook method
 
-?>
+
+  public function returnBook($book)
+  {
+    $book->returnBook();
+  }
+}
+
+
+$Book1 = new Book("The Great Gatsby", 5);
+$Book2 = new Book("To Kill a Mockingbird", 3);
+
+
+$Member1 = new Member("John Doe");
+$Member2 = new Member("Jane Smith");
+
+$Member1->borrowBook($Book1);
+$Member2->borrowBook($Book2);
+
+
+print_r("Available Copies of '{$Book1->getTitle()}': {$Book1->getAvailableCopies()} \n");
+print_r("Available Copies of '{$Book2->getTitle()}': {$Book2->getAvailableCopies()}");
